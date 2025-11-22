32db692fd5859a85655f75beee07058fe3d8fae4
Alexandru Kitsenco <64800754+alexandrukitsenco@users.noreply.github.com>
2025-11-22 18:59:23 +0100

    feat: :sparkles: update PWA configuration in vite.config.js and add application icon

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,7 +13,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
-        enabled: true
+        enabled: true,
       },
       manifest: {
         name: 'Life Game',
@@ -25,12 +23,20 @@ export default defineConfig({
         background_color: '#000000',
         display: 'standalone',
         scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
-    }),
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
